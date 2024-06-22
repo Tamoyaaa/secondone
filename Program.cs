533a1b6ac230d548ecf282ebd7dbeabee023041c
chiwauwa <chiwauwa@DESKTOP-CS7FDCL>
2024-06-23 02:57:05 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace secondone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\tОлександр");
+            Console.WriteLine("\tГливка");
+            Console.WriteLine("\tglyvka.sasha2@gmail.com");
+        }
+    }
+}
